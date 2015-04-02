ba455f34f5280a3c33936ddafc3bc42a9f1fcae6
daminal <drummersanfran@yahoo.com>
2015-04-02 13:21:13 -0400

    Generate todo list scaffold

---
+++ db/migrate/20150402021044_create_todo_lists.rb
@@ -0,0 +1,11 @@
+class CreateTodoLists < ActiveRecord::Migration
+  def change
+    create_table :todo_lists do |t|
+      t.string :title
+      t.string :string
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
