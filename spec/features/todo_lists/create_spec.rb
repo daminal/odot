5b18007734b1fb1b2a81f7ceffb307c72dbb1f9c
daminal <drummersanfran@yahoo.com>
2015-04-07 20:30:23 -0400

    merge to Brendon's code

--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
-describe "Creating todo lists" do 
-	it "redirects to the todo list index page on success" do
-		visit "/todo_lists"
-		click_link "New Todo list"
-		expect(page).to have_content("New todo_list")
+describe 'Creating todo lists' do 
+	it 'redirects to the todo list index page on success' do
+		visit todo_lists_path
+>>>>>>> 659f75ba34df93ff05df29a7f6f5d562cf455762
+		click_link 'New Todo list'
+		expect(page).to have_content('New todo_list')
 
 	end
 end
