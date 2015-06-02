fe4359a222678e79e096499c78a996e477bfde85
daminal <drummersanfran@yahoo.com>
2015-06-02 12:31:04 -0400

    fixed wrong number of arguments error using brackets not parentheses

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -22,8 +22,17 @@ class TodoItemsController < ApplicationController
 
   private
   def todo_item_params
-  	params(:todo_item).permit(:content)
+  	params[:todo_item].permit(:content)
   end
 
 
 end
+
+#Brendon caught that on line 25, I had this:   	params(:todo_item).permit(:content)
+#I needed this:   	params[:todo_item].permit(:content)
+#I got this error message: wrong number of arguments (1 for 0)
+#The technical problem was that when I use parentheses, it looks for an argument.
+#It needed instead brackets *why, I don't know. 
+#The procedural mistake I made was to view the presentation in less than full screen
+#And the brackets looked too much like parentheses. 
+#I must always use full screen mode to view the presentation. 
